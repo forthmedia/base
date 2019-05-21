abd99b908bb321b351a97a29520aeaf0e1b69b5d
Kurt Jonach <kurtjonach@Wotan.local>
2019-05-21 14:46:24 -0700

    Add Futhark font and info.

--- src/app/test-bed/test-bed.component.ts
+++ src/app/test-bed/test-bed.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ManticService } from '../services/mantic.service';
+import { SyncAsync } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'test-bed',
@@ -16,6 +17,23 @@ export class TestBedComponent implements OnInit {
   ngOnInit() {
     this.manticService = new ManticService;
     this.posibilities = this.manticService.permutation(24, 3);
+    looper(5);
   }
+}
+ 
+function looper(num) {
+  for (var i = 0; i < num; i++) {
 
+    (function(j){
+      say(j);
+    })(i);
+
+  }
+}
+
+function say(j) {
+  var interval = j * 1000;
+  setTimeout(function() {
+    console.log("The value of i is " + j);
+  }, interval);
 }
