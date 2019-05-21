a4c1bd4002b94866ce17076c259e3b88a25c6d5f
Kurt Jonach <kurtjonach@Wotan.local>
2019-05-21 16:49:43 -0700

    Fancy runic intro.

--- src/app/test-bed/test-bed.component.ts
+++ src/app/test-bed/test-bed.component.ts
@@ -11,16 +11,26 @@ export class TestBedComponent implements OnInit {
 
   manticService:ManticService;
   posibilities: number;
+  runes: string = "";
 
   constructor() { }
 
   ngOnInit() {
     this.manticService = new ManticService;
     this.posibilities = this.manticService.permutation(24, 3);
-    looper(5);
+    this.intro();
+  }
+
+  intro() {
+    var symbols: string = "futark";
+    symbols.split('');
+    for (let i = 0; i < symbols.length; i++) {
+      var interval = i * 1000;
+      setTimeout(()=> this.runes += symbols[i], interval);
+    }
   }
 }
- 
+
 function looper(num) {
   for (var i = 0; i < num; i++) {
 
