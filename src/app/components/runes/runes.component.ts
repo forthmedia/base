baafa5c3760b53d0311b8a60172e11901cb8ec82
Kurt Jonach <kurtjonach@Wotan.local>
2019-05-22 11:53:10 -0700

    Basic rune cast and button.

--- src/app/components/runes/runes.component.ts
+++ src/app/components/runes/runes.component.ts
@@ -10,8 +10,12 @@ export class RunesComponent implements OnInit {
 
   manticService:ManticService;
   posibilities: number;
-  runes: string = "";
-
+  runes: string = '\xa0';
+  lots: Array<string> = [
+    'f', 'u', 't', 'a', 'r', 'c', 'g', 'w',
+    'h', 'n', 'i', 'j', 'y', 'p', 'z', 's',
+    't', 'b', 'e', 'm', 'l', 'q', 'd', 'o'
+  ];
 
   constructor() { }
 
@@ -20,6 +24,31 @@ export class RunesComponent implements OnInit {
     this.posibilities = this.manticService.permutation(24, 3);
     this.intro();
   }
+
+  onCast() {
+    this.runes = '\xa0';
+    let one: number = -1;
+    let two: number = -1;
+    let three: number = -1;
+
+    for (let i = 0; i < 3; i++) {
+      let interval = (i+1) * 1000;
+      if (i == 1) {
+        one = Math.floor(Math.random() * 24);
+        setTimeout(()=> this.runes += this.lots[one], interval);
+      } else if (i == 2) {
+        do {
+          two = Math.floor(Math.random() * 24);
+        } while (two === one);
+        setTimeout(()=> this.runes += this.lots[two], interval);
+      } else {
+        do {
+          three = Math.floor(Math.random() * 24);
+        } while ((three == one) || (three == two));
+        setTimeout(()=> this.runes += this.lots[three], interval);
+      }
+    }
+   }
 
   intro() {
     var symbols: string = "futark";
